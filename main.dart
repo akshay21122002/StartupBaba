998d4b0dbe5d391e37dc999f499e6a331efdbc9c
akshay21122002 <101473776+akshay21122002@users.noreply.github.com>
2022-08-13 13:40:26 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './login.dart';
+import 'register.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => MyLogin(),
+        'register': (context) => MyRegister(),
+      },
+    );
+  }
+}
